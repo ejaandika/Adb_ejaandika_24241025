84e679652b5a04ac918544f1b51cf094c422ff05
Eja andika <ejaandika47@gmail.com>
2026-05-08 15:08:53 +0800

    Add files via upload

---
+++ MODUL_2/tugas II.sql
@@ -0,0 +1,21 @@
+-- Nama  : Eja Andika
+-- Nim   : 24241025
+-- kelas : A
+-- modul : Tugas 2
+
+-- Menggunakan database
+USE Pti_mart;
+
+-- Menampilkan database
+SHOW DATABASES;
+
+-- Cek isi Tabel
+SELECT * FROM pelanggan;
+SELECT * FROM produk;
+
+
+-- case 1 tim sales mencari nama customer dan alamatnya
+SELECT p.nama_pelanggan AS nama_customer, p.alamat AS alamat_customer FROM pelanggan p;
+
+-- case 2 tim sales ingin melihat nama produk dan harganya
+SELECT pr.nama_produk , pr.harga AS harga_produk FROM produk pr;
